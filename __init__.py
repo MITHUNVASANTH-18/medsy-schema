969a7c508dd301ff26a4bd41e8f1bfb75f715d6e
naveenkumar <naveenkumar@wizzgeeks.com>
2025-01-24 12:57:32 +0530

    added init file

---
+++ __init__.py
@@ -0,0 +1,9 @@
+
+from component_model import Component
+from component_association_model import Component_association
+from layer_1_model import Layer_1
+from layer_2_model import Layer_2
+from layer_3_model import Layer_3
+
+
+PACKAGE_NAME = "medsy_schema"
